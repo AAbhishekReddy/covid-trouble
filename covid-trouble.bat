36af6a003efce4c97e2659a21b25974b7896d18f
AAbhishekReddy <annemabhishekreddy.com>
2020-08-02 05:00:03 +0530

    Sun 08/02/2020

--- covid-trouble.bat
+++ covid-trouble.bat
@@ -1,32 +1,28 @@
 @echo off
 
-REM pause
-
-
 echo -------------------------------------------------------------------------------------------------------
 echo -                  Donot close this cmd or the new web browser that is gonna pop up                   - 
 echo -                  Summon GHOST if you are not sure about it.....GHOST                                -
 echo -------------------------------------------------------------------------------------------------------
 
-echo
-
-echo
+echo.
+echo.
 
 echo                                    Running the extractor....
 
 REM "C:\Users\ghost\AppData\Local\Programs\Python\Python38\python.exe" "E:\Code dump\covid\covid-trouble\support.py"
 
-REM "C:\Users\ghost\AppData\Local\Programs\Python\Python38\python.exe" "E:\Code dump\covid\covid-trouble\xwala.py"
+"C:\Users\ghost\AppData\Local\Programs\Python\Python38\python.exe" "E:\Code dump\covid\covid-trouble\xwala.py"
 
+echo.
 echo                                      File has executed....
-echo 
-
-echo 
+echo.
+echo.
 
 echo                                     Updating github repo....
-
+echo.
 echo                                     Moving into the Git Repo
-
+echo.
 cd
 
 E:
@@ -40,8 +36,8 @@ git add .
 git commit -m "%date%"
 git push origin
 
-echo                                         Git updated.
-
+echo                                            Git updated.
+echo.
 echo                                       ---  %time%  ---  
 
 pause
